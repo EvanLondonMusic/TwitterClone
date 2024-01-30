b75aabad33607e63486acceac69f2f7989684e3d
EvanLondonMusic <154972101+EvanLondonMusic@users.noreply.github.com>
2024-01-30 18:04:43 +0300

    Added LoginControllerUI

--- TwitterClone/TwitterClone/Controller/Authentication/LoginController.swift
+++ TwitterClone/TwitterClone/Controller/Authentication/LoginController.swift
@@ -18,6 +18,31 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+        view.backgroundColor = .twitterBlue
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
+        view.backgroundColor = .twitterBlue
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
     // MARK: - Lifcycle
     
     override func viewDidLoad() {
@@ -37,5 +62,13 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 100, height: 100)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 30, paddingRight: 30)
     }
 }
