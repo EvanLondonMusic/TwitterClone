39fc9b91d3a4eaa0f1b994faa27d08392147f50b
EvanLondonMusic <154972101+EvanLondonMusic@users.noreply.github.com>
2024-02-01 19:57:31 +0300

    Added login and register function

---
+++ TwitterClone/TwitterClone/API/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  TwitterClone
+//
+//  Created by Evan London on 01.02.2024.
+//
+
+import Foundation
---
+++ TwitterClone/TwitterClone/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  TwitterClone
+//
+//  Created by Evan London on 01.02.2024.
+//
+
+import Foundation
